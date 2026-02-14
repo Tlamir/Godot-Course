f3e8f7739f63a4a1ca155875e90522442188a829
Yigithan Guven <yigithanguvenn@gmail.com>
2026-02-14 23:35:32 +0300

    Add player-triggered vanish timer to platform
    Make platforms vanish only after the player steps on them: add an Area3D (PlayerDetector) with a cylinder collision shape and a body_entered signal that starts a one-shot Timer. Hook up the Timer to play the "Vanish" animation and free the node when the animation finishes; the body_entered handler disconnects itself to prevent retriggers. Also expose Timer and PlayerDetector in the script and hide the existing collision shape in the scene.

--- SkyPogo/Scenes/Platform.gd
+++ SkyPogo/Scenes/Platform.gd
@@ -1,10 +1,20 @@
 extends Node3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var timer: Timer = $Timer
+@onready var player_detector: Area3D = $PlayerDetector
 
 func _on_timer_timeout() -> void:
 	animation_player.play("Vanish")
+	
 
 
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 		queue_free()
+
+
+func _on_player_detector_body_entered(body: Node3D) -> void:
+	if body is Player:
+		timer.start()
+		# Disconnect From signal
+		player_detector.body_entered.disconnect(_on_player_detector_body_entered)
