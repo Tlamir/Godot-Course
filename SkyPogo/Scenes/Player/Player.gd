1c4f9b85feaf0ac7aea09dd9c9c00d9d4c57f31e
Yigithan Guven <yigithanguvenn@gmail.com>
2026-03-26 20:22:56 +0300

    Refactor Player physics, add animation & bones
    Extract gravity and animation handling from _physics_process into handle_gravity() and handle_animation(); call move_and_slide() from _physics_process. Add an onready AnimationPlayer reference and play "jump"/"fall" animations based on velocity.y. Update player scene skeleton bone rotations (several Quaternion changes) to adjust the character pose.

--- SkyPogo/Scenes/Player/Player.gd
+++ SkyPogo/Scenes/Player/Player.gd
@@ -4,6 +4,7 @@ class_name Player
 
 const GRAVITY: float = 15.0
 const JUMP_FORCE: float = 12.0
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _process(_delta: float) -> void:
@@ -12,11 +13,20 @@ func _process(_delta: float) -> void:
 
 #Invoked every frame 
 func _physics_process(delta: float) -> void:
-	# Falling physics then jumping
+	handle_gravity(delta)
+	move_and_slide()
+	handle_animation()
+	
+
+func handle_gravity(delta: float) -> void:
+	 #Falling physics then jumping
 	if(is_on_floor()):
 		velocity.y+=JUMP_FORCE
 	elif(!is_on_floor()):
 		velocity.y -= GRAVITY*delta
-	move_and_slide()
 	
-	
+func handle_animation() -> void:
+	if velocity.y>0:
+		animation_player.play("jump")
+	else:
+		animation_player.play("fall")
