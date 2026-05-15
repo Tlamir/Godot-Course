8d66b2d536435e29b9f6782335f314c4e5b19d61
Yigithan Guven <yigithanguvenn@gmail.com>
2026-05-15 19:39:19 +0300

    Show jewel key icon with fade-in on collect
    Hook up a new TextureRect (TextureRectKey) to the HUD to provide visual feedback when a jewel is collected. Adds an onready reference in Hud.gd and connects SignalHub.on_jewels_collected to show the key icon and play a short fade tween (modulate alpha from 0 to 1). Also initializes the TextureRect as hidden in the scene file and sets expand/stretch modes for proper layout.

--- GrannyCapers/GrannyCapers/Scenes/Hud/Hud.gd
+++ GrannyCapers/GrannyCapers/Scenes/Hud/Hud.gd
@@ -1,10 +1,18 @@
 extends Control
 @onready var label_coins: Label = $PanelContainer/HBoxContainer/HBCoin/LabelCoins
 @onready var label_jewel: Label = $PanelContainer/HBoxContainer/HBJewel/LabelJewel
+@onready var texture_rect_key: TextureRect = $PanelContainer/HBKey/TextureRectKey
 
 
 func  _enter_tree() -> void:
-	SignalHub.on_pickup_scores_updated.connect(_update_pickup_scores)	
+	SignalHub.on_pickup_scores_updated.connect(_update_pickup_scores)
+	SignalHub.on_jewels_collected.connect(func():
+		texture_rect_key.visible=true
+		var tween: Tween = create_tween()
+		tween.set_loops(0)
+		tween.tween_property(texture_rect_key,"modulate",Color(1,1,1,0),0.5)
+		tween.tween_property(texture_rect_key,"modulate",Color(1,1,1,1),0.5)
+		)
 
 
 func _update_pickup_scores(scores: PickUpScores) -> void:
